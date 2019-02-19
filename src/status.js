7414e206184ee80e6f14d15848e9b002576a00f6
Christopher Meldre Pettersen <meldre@outlook.com>
2019-02-19 14:55:07 +0100

    Update

--- src/status.js
+++ src/status.js
@@ -5,3 +5,16 @@ function updateIndicator() {
     : "badge badge-pill badge-danger";
   indicator.textContent = navigator.onLine ? "online" : "offline";
 }
+
+function ping() {
+  var ping = require("ping");
+  var hosts = ["192.168.0.100", "google.com", "yahoo.com"];
+  hosts.forEach(function(host) {
+    ping.sys.probe(host, function(isAlive) {
+      var msg = isAlive
+        ? "host " + host + " is alive"
+        : "host " + host + " is dead";
+      console.log(msg);
+    });
+  });
+}

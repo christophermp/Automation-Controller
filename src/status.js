88976b246c95b5b79719abd2970b055ada16245e
christophermp <meldre@outlook.com>
2019-02-19 18:50:41 +0100

    Update

--- src/status.js
+++ src/status.js
@@ -8,7 +8,9 @@ function updateIndicator() {
 
 function ping() {
   var ping = require("ping");
-  var hosts = ["192.168.0.100", "google.com", "yahoo.com"];
+  //var hosts = ["192.168.0.100", "google.com", "yahoo.com"];
+  var hosts = [document.getElementById("ipAddress").value];
+  // edited host.forEach
   hosts.forEach(function (host) {
     ping.sys.probe(host, function (isAlive) {
       var msg = isAlive
@@ -19,4 +21,34 @@ function ping() {
         "Pinging " + msg;
     });
   });
+}
+
+function scanner() {
+  var evilscan = require('evilscan');
+
+  var options = {
+    target: '10.0.0.161',
+    port: '21-23',
+    status: 'TROU', // Timeout, Refused, Open, Unreachable
+    banner: true
+  };
+
+  var scanner = new evilscan(options);
+
+  scanner.on('result', function (data) {
+    // fired when item is matching options
+    console.log(data);
+    document.getElementById("pingStatus").innerHTML =
+      JSON.stringify(data, undefined, 2);
+  });
+
+  scanner.on('error', function (err) {
+    throw new Error(data.toString());
+  });
+
+  scanner.on('done', function () {
+    // finished !
+  });
+
+  scanner.run();
 }
